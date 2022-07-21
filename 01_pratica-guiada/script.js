9774f39df6c16a5f13bbe51269975b7628602538
Eduardo <eduarduholiveira@gmail.com>
2022-07-21 19:00:32 -0300

    exercicios aula template

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,74 @@
 //CODIGO AQUI
+const minhaIdade = prompt("Qual a sua idade");
+const idadeAmiga = prompt("Qual a idade da sua amiga ou amigo?");
+
+const minhaIdadeEdoMeuAmigo = minhaIdade > idadeAmiga;
+ const diferencaDeIdade = minhaIdade - idadeAmiga;
+
+ console.log(diferencaDeIdade);
+
+console.log(minhaIdadeEdoMeuAmigo);
+
+
+
+const a = "vou pra praia"
+const b = "sou maior de idade"
+const c = "posso tomar uma cervejinha"
+const d = "quero passear de carro"
+
+
+
+
+
+
+// let saldo = 15
+// let saldoAtualizado = saldo + 22.4
+
+// console.log(saldoAtualizado)
+// let saldoAtualizado = saldoAtualizado - 12.34
+// console.log(saldoAtualizado)
+
+// let partePorIrmao = saldoAtualizado / 3
+// console.log(partePorIrmao)
+
+// let minhaParteAplicadaNaBolsa = partePorIrmao * 3.42
+// console.log(minhaParteAplicadaNaBolsa)
+
+// let dinheiroQueRESTOUProChiquetinho = minhaParteAplicadaNaBolsa % 3
+// console.log(dinheiroQueRESTOUProChiquetinho)
+// let operação = 2*2+2
+// console.log(operação)
+// let operaçãoComPrecedencia = 2*(2+2)
+// console.log(operaçãoComPrecedencia)
+
+// const cincoEIgualACinco = 5 === 5
+// const cincoEhDiferenteDeCinco = 5 !== 5
+// const cincoNumberEhDiferenteDeCincoString = 5 !== '5'
+// const cincoStringEhIgualDeCincoCorrido = 5 === "cinco"
+// const typeOfCincoEhIgualATypeOfVinte = typeof 5 === typeof 20
+// const typeOfCincoStringEhIgualTypeOfCincoCorrido = typeof "5" === typeof "cinco"
+// console.log(cincoEIgualACinco)
+// console.log(cincoEhDiferenteDeCinco)
+// console.log(cincoNumberEhDiferenteDeCincoString)
+// console.log(cincoStringEhIgualDeCincoCorrido)
+// console.log(typeOfCincoEhIgualATypeOfVinte)
+// console.log(typeOfCincoStringEhIgualTypeOfCincoCorrido)
+
+// const numero1 = 15
+// const numero2 = 32
+
+// console.log('O primeiro numero é igual ao segundo', numero1 === numero2)
+// console.log('O primeiro numero é menor ou igual ao segundo', numero1 <= numero2)
+// console.log('O primeiro numero é maior ao segundo', numero1 > numero2)
+// console.log('O primeiro numero é menor que segundo', numero1 < numero2)
+// const a = 'Quero dirigir um Celta 2012'
+// const b = 'Sou maior de idade'
+// const c = 'Consigo comprar o Celta por 22.000 reais'
+// const d = 'Consigo alugar o Celta'
+// const e = 'Tenho carteira de motorista'
+
+// a
+// !b
+// b && (!e)
+// b && (c || d)
+// b && e && !a
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,25 @@
 // EXERCICIO 01
 const minhaIdade = prompt('Qual sua idade?')
-const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?');
+const minhaIdadeEdoMeuAmigo = minhaIdade > idadeAmiga;
+ const diferencaDeIdade = minhaIdade - idadeAmiga;
+
+ console.log("qual a diferença de idade de você?", diferencaDeIdade);
+
+console.log("Sua idade é maior que do seu amigo?",minhaIdadeEdoMeuAmigo);
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+const MaiorDeIdade = minhaIdade > 18
+
+
+
+// !b && d
+// (b && a) && !d
+// a || d
+// c && !d
+
